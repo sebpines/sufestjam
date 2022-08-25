7d8fdad1ad7e4633f9b4adc607c37a8d609282aa
planplants <hollythomasillus@gmail.com>
2022-08-25 14:53:35 -0400

    Images added

---
+++ Assets/ArrowScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    public Camera gameCamera;
+    public float distanceToMove = 20;
+    private void OnMouseDown()
+    {
+      gameCamera.transform.position += Vector3.right * distanceToMove;
+    }
+}
---
+++ Assets/Ink/myFiles/Test.ink
@@ -0,0 +1,10 @@
+Where am I?
+    *ask around
+    Who is that?!
+    -- then <>
+    *look at room
+    this place looks gross
+    - I died
+    
+-->END
+    
