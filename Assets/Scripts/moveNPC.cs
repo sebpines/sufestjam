265830dfbfc1d30836d2806148f88eaeb0604483
sebpines <111389003+sebpines@users.noreply.github.com>
2022-09-08 16:28:37 -0400

    Update moveNPC.cs

--- Assets/Scripts/moveNPC.cs
+++ Assets/Scripts/moveNPC.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class moveNPC : MonoBehaviour
 {
     bool forward;
+    bool dialoguePlaying;
     public GameObject NPC;
     //get overlay gameObject
     public GameObject Overlay;
@@ -12,51 +13,66 @@ public class moveNPC : MonoBehaviour
     public float distanceToMoveNPC = 2;
     //distance overlay moved
     public float distanceToMoveOverlay = 10;
+    
 
     // Start is called before the first frame update
     void Start()
     {
         //NPC is not forward
         forward = false;
+        dialoguePlaying = false;
 
     }
-    public void OnMouseDown()
+    void Update()
     {
-        //if NPC is forward they and the overlay move back
-        if (forward == true)
+        if ((GameObject.Find("DialoguePanel")) == null)
+        {
+            //dialogue Playing
+            dialoguePlaying = false;
+         
+        }
+        else
+        // dialogue not playing
+        { dialoguePlaying = true;
+           
+        }
+        //on button press if textbox doesnt exist and its forward
+
+        if ((Input.GetKeyUp("space")) && dialoguePlaying == false && forward == true)
         {
             NPC.transform.Translate(Vector3.forward * distanceToMoveNPC);
             Overlay.transform.Translate(Vector3.down * distanceToMoveOverlay);
             forward = false;
             Debug.Log("back");
-        }
-        //if NPC is clicked well back they and the overlay move forward
-        else if (forward == false)
-        {
-            NPC.transform.Translate(Vector3.forward * (-distanceToMoveNPC));
-            Overlay.transform.Translate(Vector3.down * (-distanceToMoveOverlay));
-            forward = true;
-            Debug.Log("forward");
         }
     }
-    
-    /*public void OnDeactivate()
+    void OnMouseDown()
     {
-        //if NPC is forward they and the overlay move back
-        if (forward == true)
-        {   
-            NPC.transform.Translate(Vector3.forward * (-distanceToMoveNPC));
-            Overlay.transform.Translate(Vector3.down * (-distanceToMoveOverlay));
-            forward = true;
-            Debug.Log("forward");
+        if (dialoguePlaying == false)
+                {
+            //if NPC is forward they and the overlay move back
+            if (forward == true)
+            {
+                NPC.transform.Translate(Vector3.forward * distanceToMoveNPC);
+                Overlay.transform.Translate(Vector3.down * distanceToMoveOverlay);
+                forward = false;
+                Debug.Log("back");
+            }
+            //if NPC is clicked well back they and the overlay move forward
+            else if (forward == false)
+            {
+                NPC.transform.Translate(Vector3.forward * (-distanceToMoveNPC));
+                Overlay.transform.Translate(Vector3.down * (-distanceToMoveOverlay));
+                forward = true;
+                Debug.Log("forward");
+            }
+
+
+
+            Debug.Log("text exists");
         }
-        //if NPC is clicked well back they and the overlay move forward
-        else if (forward == false)
-        {
-            NPC.transform.Translate(Vector3.forward * distanceToMoveNPC);
-            Overlay.transform.Translate(Vector3.down * distanceToMoveOverlay);
-            forward = false;
-            Debug.Log("back");
-        }
-    }*/
+
+
+        
+    }
 }
