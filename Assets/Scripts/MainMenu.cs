28b0aa6d713fd102ae4925dd5ffe9586551ec0de
planplants <hollythomasillus@gmail.com>
2022-10-05 12:24:09 -0400

    main menu updated

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
-{
+
+{ 
+   
+
     public void PlayGame() 
     { 
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     // Start is called before the first frame update
     }
-    public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Quit");
-    }
+   
+       
+    
 }
+   
